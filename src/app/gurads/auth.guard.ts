250ce4d7b3ee7d9dadaa6e9acbbf9c890e5db841
Safnaj <safnaj99@gmail.com>
2021-10-11 00:30:57 +0530

    Auth Gurd

---
+++ src/app/gurads/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> | boolean {
+    return this.auth.user$.pipe(
+      map((user) => {
+        if (user) return true;
+
+        this.router.navigate(['/']);
+        return false;
+      })
+    );
+  }
+}
