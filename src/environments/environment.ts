3f0e6237834c95e321a16a799e55e100c8e12632
Safnaj <safnaj99@gmail.com>
2021-10-11 00:32:03 +0530

    Post Service & Endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,6 +12,8 @@ export const environment = {
     messagingSenderId: '95976574440',
     appId: '1:95976574440:web:00e2f4f8d28978c7dfe85d',
   },
+  apiURL: 'https://code-kitty-prep-api.herokuapp.com/api/v1',
+  postAPIPath: 'post',
 };
 
 /*
