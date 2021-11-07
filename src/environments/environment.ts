907616aa158e176a6e3d9d1f9415371829e73961
Safnaj <safnaj99@gmail.com>
2021-11-08 00:29:16 +0530

    API Endpoints & Paths

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,8 +12,10 @@ export const environment = {
     messagingSenderId: '95976574440',
     appId: '1:95976574440:web:00e2f4f8d28978c7dfe85d',
   },
-  apiURL: 'https://code-kitty-prep-api.herokuapp.com/api/v1',
+  apiURL: 'https://code-kitty-api.herokuapp.com/api/v1',
   postAPIPath: 'post',
+  categoryAPIPath: 'category',
+  userAPIPath: 'user',
 };
 
 /*
