ad7346f24c80cbf4b9295b634166cd9d5914cc96
Safnaj <safnaj99@gmail.com>
2021-11-08 00:28:27 +0530

    User Profile

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
-  constructor() {}
+export class NavbarComponent {
+  user: User | any;
 
-  ngOnInit(): void {}
+  constructor(
+    private afAuth: AngularFireAuth,
+    private authService: AuthService
+  ) {
+    afAuth.authState.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
