9d80e96c3e04a1e3ed4af78fd0783568880fae08
Aashif Ahamed <aashifahamed0796@gmail.com>
2021-11-27 14:27:04 +0530

    Show Real Post-Data Issue fixed

--- src/app/components/feed/post-card/post-card.component.ts
+++ src/app/components/feed/post-card/post-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Post } from 'src/app/models/post.interface';
+import { Post } from 'src/app/models/post.model';
 
 @Component({
   selector: 'app-post-card',
