482e1367ca917ee0c517748bbcde8a972d21ad1f
Safnaj <safnaj99@gmail.com>
2021-11-28 20:58:41 +0530

    CI Issue Fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,6 +8,8 @@ export const environment = {
     messagingSenderId: '95976574440',
     appId: '1:95976574440:web:00e2f4f8d28978c7dfe85d',
   },
-  apiURL: 'https://code-kitty-prep-api.herokuapp.com/api/v1',
+  apiURL: 'https://code-kitty-api.herokuapp.com/api/v1',
   postAPIPath: 'post',
+  categoryAPIPath: 'category',
+  userAPIPath: 'user',
 };
