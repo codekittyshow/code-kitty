a0ae306fb8031c5a1f6e3731d4b205d72dd1ae9f
Shehanka <hcsperera@outlook.com>
2022-01-30 19:03:31 +0530

    Env Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-require('dotenv').config();
+const API_HOST = 'http://20.120.64.178:8080'
 
 export const environment = {
   production: false,
@@ -10,7 +10,7 @@ export const environment = {
     messagingSenderId: '95976574440',
     appId: '1:95976574440:web:00e2f4f8d28978c7dfe85d',
   },
-  apiURL: process.env.API_URL || 'https://code-kitty-api.herokuapp.com/api/v1',
+  apiURL: `${API_HOST}/api/v1`,
   postAPIPath: 'post',
   categoryAPIPath: 'category',
   userAPIPath: 'user',
